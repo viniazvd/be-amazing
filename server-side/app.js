ec065a39bceda290e30bfc139216d6dd8b82912a
viniazvd <viniazvd@gmail.com>
2017-07-28 02:04:54 -0300

    vue starting

--- server-side/app.js
+++ server-side/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const users = require('./routes/users')
 
@@ -21,11 +22,22 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(cors())
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
+
 app.get('/ping', (req, res) => {
   res.send('pong')
 })
 
 app.use('/users', users)
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
